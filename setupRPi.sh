4d5eee856e0a261b21b8e8bb46f5b76c868c78d3
Ismail Arai <ismail@isb2-dhcp-170-219.naist.jp>
2022-04-26 13:19:54 +0900

    add git to install

--- setupRPi.sh
+++ setupRPi.sh
@@ -4,5 +4,5 @@ sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_change_locale en_US.UTF-8
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install zsh lv i2c-tools python3-smbus python3-pip
+sudo apt -y install zsh lv i2c-tools git python3-smbus python3-pip
 pip3 install -r requirements.txt
