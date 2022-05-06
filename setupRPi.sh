389e19d3e4743449a8cf8c8eeb345ebf45d27a59
ismail-a <ismail@itc.naist.jp>
2022-05-06 11:51:21 +0900

    ename serial console

--- setupRPi.sh
+++ setupRPi.sh
@@ -2,6 +2,7 @@
 sudo raspi-config nonint do_boot_behaviour B1
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_change_locale en_US.UTF-8
+sudo raspi-config nonint do_serial 0
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install zsh lv i2c-tools git python3-smbus python3-pip
